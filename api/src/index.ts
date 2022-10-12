cfe86874ced1e74db83bda7c7e6f056b1598fbfe
Tiago <tiago.freitas.faria@gmail.com>
2022-10-12 18:43:12 -0300

    criação da API

---
+++ api/src/index.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT ?? 3333
+
+app.listen(port, () => {
+	console.log(`server running on port ${port}`)
+})
