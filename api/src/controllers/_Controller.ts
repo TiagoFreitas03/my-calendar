132fd1da0be0558153254245fbc29a743a25dc6b
Tiago Freitas <tiago.freitas.faria@gmail.com>
2022-10-13 08:38:10 -0300

    criação da classe Controller

---
+++ api/src/controllers/_Controller.ts
@@ -0,0 +1,45 @@
+import { Response, Request, NextFunction, Router } from 'express'
+
+/** função middleware */
+type TMiddleware = (req: Request, res: Response, next: NextFunction) => Promise<void>
+
+/** propriedades da rota */
+interface IRoute {
+	/** caminho (url) */
+	path: string
+	/** método HTTP */
+	method: 'get' | 'post' | 'patch' | 'put' | 'delete'
+	/** função que vai receber a requisição e devolver a resposta */
+	handler: (req: Request, res: Response) => Promise<Response>
+	/** middlewares da rota */
+	middlewares: TMiddleware[]
+}
+
+/** controller base para implementação das rotas */
+export abstract class Controller {
+	/** primeira parte da url da rota */
+	private prefix: string
+
+	/** instância de Router para mapeamento das rotas */
+	private router: Router
+
+	/** array de objetos que implementam a interface IRoute */
+	protected routes: IRoute[] = []
+
+	/**
+	 * instancia o controller e inicia o router
+	 * @param prefix primeira parte da rota
+	 */
+	constructor(prefix?: string) {
+		this.router = Router()
+		this.prefix = prefix ?? ''
+	}
+
+	/** configura método HTTP, middlewares, e função para cada rota. Retorna um objeto Router */
+	setRoutes() {
+		for (const route of this.routes)
+			this.router[route.method](this.prefix + route.path, ...route.middlewares, route.handler)
+
+		return this.router
+	}
+}
