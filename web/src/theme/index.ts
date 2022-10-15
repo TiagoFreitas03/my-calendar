e3f5eceb91a6be4800249a3a393551850fbe76fc
Tiago <tiago.freitas.faria@gmail.com>
2022-10-15 17:18:17 -0300

    objeto com as cores tema da aplicação

---
+++ web/src/theme/index.ts
@@ -0,0 +1,17 @@
+/** paleta das cores principais */
+export const COLORS = {
+	/** amarelo */
+	YELLOW: '#FFBE0B',
+
+	/** laranja */
+	ORANGE: '#FB5607',
+
+	/** rosa */
+	PINK: '#FF006E',
+
+	/** roxo */
+	PURPLE: '#8338EC',
+
+	/** azul */
+	BLUE: '#3A86FF',
+}
