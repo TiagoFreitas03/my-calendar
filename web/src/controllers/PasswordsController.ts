4b814ad9e89ec9828d46f1771fe5be17e0ebf960
Tiago <tiago.freitas.faria@gmail.com>
2022-10-15 19:21:18 -0300

    criação do método retrieve

--- web/src/controllers/PasswordsController.ts
+++ web/src/controllers/PasswordsController.ts
@@ -3,6 +3,16 @@ import { api } from '../services/api'
 /** controller de senhas */
 export class PasswordsController {
 	/**
+	 * envia e-mail de recuperação de senha para o usuário
+	 * @param email e-mail do usuário
+	 */
+	async retrieve(email: string) {
+		const res = await api.post('password/forgot', { email })
+
+		return res.data.message as string
+	}
+
+	/**
 	 * envia à API os dados para atualização de senha
 	 * @param current senha atual
 	 * @param newPassword nova senha
