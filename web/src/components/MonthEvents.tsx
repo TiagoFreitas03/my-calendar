0a19917ea90e87be569e76317e53968aa9d60234
Tiago <tiago.freitas.faria@gmail.com>
2022-10-15 17:19:13 -0300

    componente para exibição dos eventos no mês/ano

---
+++ web/src/components/MonthEvents.tsx
@@ -0,0 +1,9 @@
+export function MonthEvents() {
+	return (
+		<div
+			className='lg:block hidden p-4 rounded bg-gray-700 w-[25%] overflow-y-auto max-h-[1080px]'
+		>
+			<p>Faça login para visualizar seus próximos eventos</p>
+		</div>
+	)
+}
