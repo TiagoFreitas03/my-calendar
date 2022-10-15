be56cf8700fe140633c65493bec38080ea3c3f54
Tiago <tiago.freitas.faria@gmail.com>
2022-10-15 17:19:28 -0300

    componente de botão com ícone

---
+++ web/src/components/IconButton.tsx
@@ -0,0 +1,20 @@
+import { ButtonHTMLAttributes } from 'react'
+
+interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	icon: string
+	color: 'blue' | 'purple' | 'pink' | 'orange' | 'yellow'
+}
+
+export function IconButton({ icon, color, ...rest }: IconButtonProps) {
+	return (
+		<button
+			className={
+				'w-12 h-12 rounded-lg mx-1 icon-button '+
+				`bg-${color}-500 hover:bg-${color}-600 transition-colors`
+			}
+			{...rest}
+		>
+			<i className={`fa-solid fa-${icon}`} />
+		</button>
+	)
+}
