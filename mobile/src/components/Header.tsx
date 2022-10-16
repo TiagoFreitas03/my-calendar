062a12d0fe0a4ed46fba99091154d4e30317eca2
Tiago <tiago.freitas.faria@gmail.com>
2022-10-16 16:49:25 -0300

    componente Header

---
+++ mobile/src/components/Header.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet, View, Image, Text, Pressable } from 'react-native'
+import Icon from '@expo/vector-icons/Ionicons'
+
+import { logo } from '../assets'
+import { COLORS, FONT_FAMILY, FONT_SIZE } from '../theme'
+
+export function Header() {
+	return (
+		<View style={styles.container}>
+			<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+				<Image source={logo} style={styles.image} />
+
+				<Text style={styles.title}>MyCalendar</Text>
+			</View>
+
+			<Pressable>
+				<Icon name='menu' size={30} color={COLORS.GRAY_100} />
+			</Pressable>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: COLORS.GRAY_700,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		padding: 20,
+		borderBottomWidth: 2,
+		borderColor: COLORS.GRAY_600
+	},
+
+	image: {
+		width: 40,
+		height: 31.5
+	},
+
+	title: {
+		color: COLORS.GRAY_100,
+		fontSize: FONT_SIZE.LG,
+		marginLeft: 12,
+		fontFamily: FONT_FAMILY.BOLD
+	}
+})
