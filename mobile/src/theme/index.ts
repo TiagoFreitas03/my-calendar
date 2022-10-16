f514bf15f9231e2de498dd8ce703dc3334fb53a0
Tiago <tiago.freitas.faria@gmail.com>
2022-10-16 16:49:03 -0300

    criação do tema (cores, fontes e tamanhos)

---
+++ mobile/src/theme/index.ts
@@ -0,0 +1,62 @@
+/** cores */
+const COLORS = {
+	/** cinza nível 100 */
+	GRAY_100: '#E1E1E6',
+	/** cinza nível 200 */
+	GRAY_200: '#C4C4CC',
+	/** cinza nível 300 */
+	GRAY_300: '#8D8D99',
+	/** cinza nível 500 */
+	GRAY_500: '#323238',
+	/** cinza nível 600 */
+	GRAY_600: '#29292E',
+	/** cinza nível 700 */
+	GRAY_700: '#121214',
+	/** cinza nível 900 */
+	GRAY_900: '#09090A',
+
+	/** amarelo nível 500 */
+	YELLOW_500: '#FFBE0B',
+	/** amarelo nível 600 */
+	YELLOW_600: '#EEAD0A',
+
+	/** laranja nível 500 */
+	ORANGE_500: '#FB5607',
+	/** laranja nível 600 */
+	ORANGE_600: '#EA4506',
+
+	/** rosa nível 500 */
+	PINK_500: '#FF006E',
+	/** rosa nível 600 */
+	PINK_600: '#EE005D',
+
+	/** roxo nível 500 */
+	PURPLE_500: '#8338EC',
+	/** roxo nível 600 */
+	PURPLE_600: '#7227DB',
+
+	/** azul nível 500 */
+	BLUE_500: '#3A86FF',
+	/** azul nível 600 */
+	BLUE_600: '#2975EE'
+}
+
+/** tipo de fonte */
+const FONT_FAMILY = {
+	/** negrito */
+	BOLD: 'Nunito_700Bold',
+	/** normal */
+	REGULAR: 'Roboto_400Regular'
+}
+
+/** tamanho da fonte */
+const FONT_SIZE = {
+	/** pequena */
+	SM: 16,
+	/** média */
+	MD: 18,
+	/** grande */
+	LG: 20
+}
+
+export { FONT_FAMILY, FONT_SIZE, COLORS }
