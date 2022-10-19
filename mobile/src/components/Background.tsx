f811d0363ee7bbb46e681ac693c8783096acd458
Tiago <tiago.freitas.faria@gmail.com>
2022-10-18 22:29:19 -0300

    criação do componente Background

---
+++ mobile/src/components/Background.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react'
+import { Dimensions, ScrollView, StyleSheet } from "react-native"
+
+import { COLORS } from '../theme'
+
+/** propriedades do fundo */
+interface BackgroundProps {
+	/** indica se o conteúdo será centralizado */
+	center?: boolean
+
+	/** elementos filhos */
+	children: ReactNode
+}
+
+/** fundo das telas */
+export function Background({ center, children }: BackgroundProps) {
+	return (
+		<ScrollView
+			contentContainerStyle={[
+				styles.container,
+				center && {
+					flexGrow: 1,
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: Dimensions.get('window').height
+				}
+			]}
+		>
+			{ children }
+		</ScrollView>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: COLORS.GRAY_900,
+	}
+})
