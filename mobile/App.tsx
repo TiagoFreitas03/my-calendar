ec23d1170699fbc5223d43ef70cef4426560e0d9
Tiago <tiago.freitas.faria@gmail.com>
2022-10-21 01:17:00 -0300

    alteração no carregamento; uso das rotas e contexto de autenticação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,26 +1,25 @@
-import { StatusBar, View, Text } from "react-native"
+import 'react-native-gesture-handler'
+
+import { StatusBar } from "react-native"
 import { useFonts as useNunito, Nunito_700Bold } from '@expo-google-fonts/nunito'
 import { useFonts as useRoboto, Roboto_400Regular } from '@expo-google-fonts/roboto'
 
-import { Home } from './src/screens/Home'
+import { AuthContextProvider } from './src/contexts/AuthContext'
+import { Routes } from './src/routes/index.routes'
+import { Loading } from './src/screens/Loading'
 
 export default function App() {
 	const [nunitoLoaded] = useNunito({ Nunito_700Bold })
 	const [robotoLoaded] = useRoboto({ Roboto_400Regular })
 
-	if (!(nunitoLoaded && robotoLoaded)) {
-		return (
-			<View>
-				<Text>carregando</Text>
-			</View>
-		)
-	}
+	if (!(nunitoLoaded && robotoLoaded))
+		return <Loading />
 
 	return (
-		<>
-			<Home />
+		<AuthContextProvider>
+			<Routes />
 
 			<StatusBar />
-		</>
+		</AuthContextProvider>
 	)
 }
