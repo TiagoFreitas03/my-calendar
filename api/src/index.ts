e3a8baad870bee22d9c3cc3c3b76f7631f7aafd3
Tiago <tiago.freitas.faria@gmail.com>
2022-11-05 00:13:07 -0300

    exclusão dos arquivos relacionados à Label

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,6 @@ import { Server } from './Server'
 import { toNumber } from './utils/convertions'
 
 import { UsersController } from './controllers/UsersController'
-import { LabelsController } from './controllers/LabelsController'
 import { EventsController } from './controllers/EventsController'
 import { PasswordsController } from './controllers/PasswordsController'
 import { DatesController } from './controllers/DatesController'
@@ -13,7 +12,6 @@ const port = toNumber(process.env.PORT, 3333)
 
 const controllers = [
 	new UsersController(),
-	new LabelsController(),
 	new EventsController(),
 	new PasswordsController(),
 	new DatesController()
