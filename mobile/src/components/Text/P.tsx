aca96bcd9ca6541cee30674dbf73de627c076725
Tiago <tiago.freitas.faria@gmail.com>
2022-11-07 22:55:56 -0300

    inclusão de props para o parágrafo

--- mobile/src/components/Text/P.tsx
+++ mobile/src/components/Text/P.tsx
@@ -3,14 +3,23 @@ import { Text } from "react-native"
 import { BaseProps } from "../../interfaces/BaseProps"
 import { COLORS, FONT_FAMILY, FONT_SIZE } from "../../theme"
 
+/** propriedades do parágrafo */
+interface ParagraphProps extends BaseProps {
+	/** indica se o texto deve ser centralizado */
+	centered?: boolean
+	/** cor do texto */
+	color?: string
+}
+
 /** parágrafo (texto com estilo padrão) */
-export function P({ children }: BaseProps) {
+export function P({ children, centered, color = COLORS.GRAY_100 }: ParagraphProps) {
 	return (
 		<Text
 			style={{
-				color: COLORS.GRAY_100,
+				color,
 				fontSize: FONT_SIZE.SM,
-				fontFamily: FONT_FAMILY.REGULAR
+				fontFamily: FONT_FAMILY.REGULAR,
+				textAlign: centered ? 'center' : 'left',
 			}}
 		>
 			{ children }
